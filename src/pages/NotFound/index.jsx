1d108164d357fa60481123e89b5022e9af071ef2
DanielMC <matinzcorndani@hotmail.com>
2023-08-08 16:13:26 -0600

    Developing notFound pages

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import './NotFound.css'
+import { useParams } from "react-router-dom";
 
 function NotFound(){
     return(
-        <p>Not found</p>
+        <main className="main-notfound">
+            <h1 className="main-notfound--title">Página no encontrada</h1>
+            <p className="main-notfound--sad">:(</p>
+        </main>
     );
 }
 
