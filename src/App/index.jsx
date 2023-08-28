386d31bf1ee5b8e5346dac04be39f872197b048f
DanielMC <matinzcorndani@hotmail.com>
2023-08-28 12:50:03 -0600

    Creating general chat for phone UI and updating notification component for this feature

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -7,6 +7,8 @@ import { NotFound } from '../Components/NotFound';
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import { supabase } from '../supabase/client';
 import { Loading } from './pages/Loading';
+import { General } from './pages/General';
+import { ContextProvider } from '../Desktop/context';
 
 export default function App() {
 
@@ -26,8 +28,9 @@ export default function App() {
         <Route path='/' element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/loading' element={<Loading />} />
-        <Route path='/conversations/:slug' element={<Chat />} />
-        <Route path='/conversations' element={<Chats />} />
+        <Route path='/conversations' element={<ContextProvider><Chats /></ContextProvider>} />
+        <Route path='/conversations/general' element={<ContextProvider><General/></ContextProvider>} />
+        <Route path='/conversations/:slug' element={<ContextProvider><Chat /></ContextProvider>} />
         <Route path='/notfound' element={<NotFound />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
--- src/App/pages/General/index.jsx
+++ src/App/pages/General/index.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { supabase } from '../../../supabase/client';
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import { PopUp } from '../../../modals/PopUp'
 import { ConnectionLost } from '../../../Components/ConnectionLost'
 import { useOnLine } from '../../../Hooks/useOnLine';
-import { Context } from '../../context';
-import './chat.css'
+import { Context } from '../../../Desktop/context';
+import { FaAngleLeft } from '@react-icons/all-files/fa/FaAngleLeft';
+import { Notification } from '../../../Components/Notification';
+import './Chat.css'
 
 function General() {
-
     const { userId, socket, name, generalMsg, saveGeneralMsg } = React.useContext(Context);
-    const {slug} = useParams()
+    const { slug } = useParams()
+    let idTimeOut;
     const { isOnline } = useOnLine();
     const [message, setMessage] = useState(''); //aqui
-    const [messages, setMessages] = useState([]);
+    const [notification, setNotification] = useState(null)
 
     function msgs(msg) { //para que no se reinicie messages cada vez que se le agregue algo
         saveGeneralMsg(msg)
@@ -45,6 +47,20 @@ function General() {
     }, [navigate]); //Al montarlo y una vez que cabie navigate
 
     useEffect(() => {
+        socket.on('message', msg => { // ese msg será el json recibido
+            clearTimeout(idTimeOut);
+            setNotification(msg);
+            idTimeOut = setTimeout(() => {
+                setNotification(null);
+            }, 5000);
+        })
+
+        return () => {
+            socket.off('message')
+        }
+    }, []);
+
+    useEffect(() => {
         console.log('Efecto de socket on general');
         socket.on('general', msg => { // ese msg será el json recibido
             saveGeneralMsg(msg);
@@ -57,33 +73,39 @@ function General() {
     }, []); //Después de montarlo y cada vez que cambie num
 
     return (
-        <div className="desktop-main-container">
-            <header className="desktop-chat-header-container">
-                <img className="desktop-chat-info--photo--chat" src='https://avatars.githubusercontent.com/u/142632036?s=200&v=4' />
-                <span className="desktop-header-username">Incluyo general</span>
+        <div className="main-container">
+            {(notification && <Notification chat={notification} general={false} />)}
+            <header className="chat-header-container">
+                <Link
+                    className="chat-header--back"
+                    to={'/conversations'}>
+                    <FaAngleLeft className="chat-header--bacl--arrow" />
+                </Link>
+                <img className="chat-info--photo--chat" src='https://avatars.githubusercontent.com/u/142632036?s=200&v=4' />
+                <span className="header-username">Incluyo general</span>
             </header>
-            <ul className="desktop-chat-container">
+            <ul className="chat-container">
                 {
                     generalMsg.map((message, i) => (
                         <li
                             className={
-                                message.from != userId ? "desktop-chat-container--message--recieved" : "desktop-chat-container--message"}
+                                message.from != userId ? "chat-container--message--recieved" : "chat-container--message"}
                             key={i}>
-                                <p>{message.name}<br/>{message.text}</p>
-                                </li>
+                            <p>{message.name}<br />{message.text}</p>
+                        </li>
                     )
                     )
                 }
             </ul>
             <form
-                className="desktop-type-message">
+                className="type-message">
                 <input
                     value={message}
-                    className="desktop-type-message-editor"
+                    className="type-message-editor"
                     type='text' onChange={(e) => setMessage(e.target.value)} />
                 <button
                     type="submit"
-                    className="desktop-type-message--button"
+                    className="type-message--button"
                     onClick={handlerSend}>
                     Send
                 </button>
