870e3953a43de60310683f9d056a87f54b8f962e
DanielMC <matinzcorndani@hotmail.com>
2023-09-21 23:51:01 -0600

    Create welcome page

--- src/Desktop/components/Welcome/index.jsx
+++ src/Desktop/components/Welcome/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Welcome.css';
+import { Context } from '../../context';
 
 function Welcome() {
-    return (
-    <h1>Página de bienvenida</h1>
-      )
+  const { name } = React.useContext(Context);
+  return (
+    <>
+      <div className='bg-rainbow'></div>
+      <h1 className='welcome-text'>Bienvenidx, {name}</h1>
+      <h2 className='welcome-link'>Quienes somos</h2>
+      <h2 className='welcome-link'>Políticas de privacidad</h2>
+
+      <div className='hexagon'></div>
+    </>
+  )
 }
 
-export {Welcome}
+export { Welcome }
