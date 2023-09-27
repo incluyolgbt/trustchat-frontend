c755ebbc877b31e93b236438fd123cfce574a24b
estradae <estradae@ae.com>
2023-09-27 08:49:22 -0600

    add link to admin

--- src/Desktop/components/Welcome/index.jsx
+++ src/Desktop/components/Welcome/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Welcome.css';
 import { Context } from '../../context';
+import { Link } from 'react-router-dom';
 
 function Welcome() {
   const { name } = React.useContext(Context);
@@ -11,9 +12,13 @@ function Welcome() {
       <h2 className='welcome-link'>Quienes somos</h2>
       <h2 className='welcome-link'>Políticas de privacidad</h2>
 
+      <Link to={'/admin'}>
+        <h2 className='welcome-link'>Gestión de Chats</h2>
+      </Link>
+
       <div className='hexagon'></div>
     </>
-  )
+  );
 }
 
-export { Welcome }
+export { Welcome };
